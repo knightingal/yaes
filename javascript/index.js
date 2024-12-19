a1987f3f9b10e449446f3e65ed568717eec8e122
knightingal <yujianming1985@hotmail.com>
2024-12-19 23:06:48 +0800

    implement `shiftRows`

--- javascript/index.js
+++ javascript/index.js
@@ -5,9 +5,22 @@
 
   }
 
-  const shiftRows = () => {
-
+  const shiftRows = (d) => {
+    const result = [[], [], [], [],];
+    for (let r = 0; r < 4; r++) {
+      for (let c = 0; c < 4; c++) {
+        result[r][c] = d[r][(c + r) % 4];
+      }
+    }
+    return result;
   }
+
+  console.log(shiftRows([
+    [0,1,2,3,],
+    [4,5,6,7,],
+    [8,9,10,11],
+    [12,13,14,15],
+  ]));
 
   const mixColumns = () => {
 
