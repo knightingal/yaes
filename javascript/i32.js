bc07b47e971b70ad2ee02484aca8160874cf2740
knightingal <yujianming1985@hotmail.com>
2024-12-25 23:08:49 +0800

    implement `mixColumns`

--- javascript/i32.js
+++ javascript/i32.js
@@ -1,3 +1,5 @@
+const { gfMulBy2Map, gfMulBy3Map, gfMulBy1Map, toHex } = require("./base");
+
 (() => {
   console.log("start");
   const byteArrayToInt32 = (ba) => {
@@ -12,7 +14,18 @@
     return result;
   };
 
-  
+  const mixColumns = (i32) => {
+    const result0 = gfMulBy2Map[(i32 >> 24) & 0xff] ^ gfMulBy3Map[(i32 >> 16) & 0xff] ^ gfMulBy1Map[(i32 >> 8) & 0xff] ^ gfMulBy1Map[i32 & 0xff];
+    const result1 = gfMulBy1Map[(i32 >> 24) & 0xff] ^ gfMulBy2Map[(i32 >> 16) & 0xff] ^ gfMulBy3Map[(i32 >> 8) & 0xff] ^ gfMulBy1Map[i32 & 0xff];
+    const result2 = gfMulBy1Map[(i32 >> 24) & 0xff] ^ gfMulBy1Map[(i32 >> 16) & 0xff] ^ gfMulBy2Map[(i32 >> 8) & 0xff] ^ gfMulBy3Map[i32 & 0xff];
+    const result3 = gfMulBy3Map[(i32 >> 24) & 0xff] ^ gfMulBy1Map[(i32 >> 16) & 0xff] ^ gfMulBy1Map[(i32 >> 8) & 0xff] ^ gfMulBy2Map[i32 & 0xff];
+    return (result0 << 24) | (result1 << 16) | (result2 << 8) | result3;
+  }
+
+  console.log(toHex(mixColumns(0x00010203), 32));
+  console.log(toHex(mixColumns(0x04050607), 32));
+  console.log(toHex(mixColumns(0x08090a0b), 32));
+  console.log(toHex(mixColumns(0x0c0d0e0f), 32));
 
   console.log("end");
 })();
