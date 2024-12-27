4c7298a324f59c5be3b69d898e2e4445c0869b86
knightingal <yujianming1985@hotmail.com>
2024-12-27 15:55:27 +0800

    implement `keyExpansion` for i32

--- javascript/i32.js
+++ javascript/i32.js
@@ -48,6 +48,75 @@ const { gfMulBy2Map, gfMulBy3Map, gfMulBy1Map, toHex, T1, T2, T3, T4, TT1, TT2, 
     return result;
   }
 
+  const Nk = 4;
+  const Nr = 10;
+  const Rcon = [
+    [],
+    [0x01000000],
+    [0x02000000],
+    [0x04000000],
+    [0x08000000],
+    [0x10000000],
+    [0x20000000],
+    [0x40000000],
+    [0x80000000],
+    [0x1b000000],
+    [0x36000000],
+  ];
+
+  const keyExpansion = (key) => {
+    const addWord = (w1, w2) => {
+      return w1 ^ w2;
+    };
+    const subWord = (w) => {
+      return (
+        sBox[(w >> 24) & 0xff] << 24 |
+        sBox[(w >> 16) & 0xff] << 16 |
+        sBox[(w >>  8) & 0xff] <<  8 |
+        sBox[ w        & 0xff] 
+      );
+    };
+
+    const rotWord = (w) => {
+      return ((w << 8) & 0xffffff00) | ((w >> 24) & 0xff);
+    };
+
+    const printWord = (w) => {
+      return toHex(w, 32);
+    }
+
+    const w = [];
+    let i = 0;
+    while (i <= Nk - 1) {
+      w[i] = 
+        (key[4 * i    ] << 24) | 
+        (key[4 * i + 1] << 16) | 
+        (key[4 * i + 2] <<  8) | 
+        (key[4 * i + 3]);
+      console.log(`${i}: ${printWord(w[i])}`);
+      i++;
+    }
+    while (i <= 4 * Nr + 3) {
+      let temp = w[i - 1];
+      console.log(`${i} temp: ${printWord(temp)}`);
+      if (i % Nk == 0) {
+        temp = rotWord(temp);
+        console.log(`${i} temp after rot: ${printWord(temp)}`);
+        temp = subWord(temp);
+        console.log(`${i} temp after sub: ${printWord(temp)}`);
+        console.log(`${i} rcon: ${printWord(Rcon[i / Nk])}`);
+        temp = addWord(temp, Rcon[i / Nk]);
+      } else if (Nk > 6 && i % Nk == 4) {
+        temp = subWord(temp);
+      }
+      w[i] = addWord(w[i - Nk], temp);
+      console.log(`${i}: ${printWord(w[i])}`);
+      i++;
+    }
+    return w;
+  };
+
+
   const mixColumns = (i32) => {
     const c0 = (i32 >> 24) & 0xff;
     const c1 = (i32 >> 16) & 0xff;
